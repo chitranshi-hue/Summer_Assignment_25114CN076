f31ce76bd9485f1719cd281da827665098049cca
chitranshi <chitranshisahu10@gmail.com>
2026-06-10 19:52:52 +0530

    updated files

--- ques5.c
+++ ques5.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int num, sum = 0, digit;
+    int num, digit, sum = 0;
 
-    // Input number
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // Calculate sum of digits
     while(num != 0) {
-        digit = num % 10;   // get last digit
-        sum = sum + digit;  // add to sum
-        num = num / 10;     // remove last digit
+        digit = num % 10;  // Extract last digit
+        sum = sum + digit; // Add digit to sum
+        num = num / 10;    // Remove last digit
     }
 
-    // Display result
-    printf("Sum of digits = %d\n", sum);
+    printf("Sum of digits = %d", sum);
 
     return 0;
 }
