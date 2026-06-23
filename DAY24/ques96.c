8294ea7eee2f0ead70d8cf77c0fd7fafb42d0ff6
chitranshi <chitranshisahu10@gmail.com>
2026-06-23 19:11:50 +0530

    uploaded

---
+++ DAY24/ques96.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, j;
+    int visited[256] = {0};
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("String after removing duplicates: ");
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] != '\n' && !visited[(unsigned char)str[i]]) {
+            printf("%c", str[i]);
+            visited[(unsigned char)str[i]] = 1;
+        }
+    }
+
+    return 0;
+}
